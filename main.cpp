598e2bac32c6767efcc9d6f56f6500afc1adcf02
superuserdevmoon <41029725+superuserdevmoon@users.noreply.github.com>
2018-07-30 19:33:10 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,197 +1,92 @@
 #include <pcap.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#define LOBYTE(x) ((unsigned char)x)
-#define HIBYTE(x) ((unsigned short)x >> 8 & 0xFF)
+#include <net/ethernet.h> // 이더넷 헤더 구조체
+#include <netinet/ip.h>   // 아이피v4 헤더 구조체
+#include <netinet/in.h>  
+#include <netinet/tcp.h>  // tcp 헤더 
+#include <arpa/inet.h>
 
-#define MAKEWORD(x, y) ((unsigned char)x | (unsigned short)y << 8)
-#define MAKEDWORD(x, y) ((unsigned short)x | (unsigned long)y << 16)
-
-
+#define PRINT_MAC "%s - %02x:%02x:%02x:%02x:%02x:%02x\n"  // 맥주소 출력함수 만들기
 
 void usage() {
-   printf("syntax: pcap_test <interface>\n");
-   printf("sample: pcap_test wlan0\n");
+    printf("syntax: pcap_test <interface>\n");
+    printf("sample: pcap_test wlan0\n");
 }
 
-
-typedef struct Ethernet_Header // 이더넷 헤더 구조체
-{
-   u_char desc[6];
-   u_char src[6];
-   short int ptype;
-}Ethernet_Header;
-
+void printMAC(const char* msg, unsigned char* target) {
+    printf(PRINT_MAC, msg, target[0], target[1], target[2], target[3], target[4], target[5]);
+}
 
 int main(int argc, char* argv[]) {
-   if (argc != 2) {
-      usage();
-      return -1;
-   }
-
-   char* dev = argv[1];
-   char errbuf[PCAP_ERRBUF_SIZE];
-
-   pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
-  
-   if (handle == NULL) {
-      fprintf(stderr, "couldn't open device %s: %s\n", dev, errbuf);
-      
-      return -1;
-   }
-
-
-   while (true) {
-      struct pcap_pkthdr* header;
-      
-      const u_char* packet;
-      int res = pcap_next_ex(handle, &header, &packet);
-    
-      if (res == 0) {
-         continue;
-      }
-
-      if (res == -1 || res == -2) {
-         break;
-      }
-
-      //printf("ETHER TYPE : 0x");
-
-      if(!((int)packet[12] == 0x08 & (int)packet[13] == 0x00 & (int)packet[23] == 6)) {
-          continue;
-      }
-
-    printf("[PACKET DATA] %u bytes captured\n\n", header->caplen);
-
-    
-
-    // Packet Data Print
-    for(int i=0; i < header->caplen; i++) {
-        printf("%02x|", packet[i]);
-
-        if(i!=0 && (i+1) %16 == 0){
-
-             printf("\n");
-        }
- 
+    if (argc != 2) {
+        usage();
+        return -1;
     }
 
-    printf("\n\n\n");
- 
-    printf("Destination MAC ADDRESS : ");
+    char* dev = argv[1];
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
 
-    for(int i=0; i < 6; i++) {
-        printf("%02x", packet[i]);
+    int index = 0;
 
-        if(i != 5) {
-            printf(":");
-        }
-
-        if(i!=0 && (i+1) %16 == 0){
-
-             printf("\n");
-        }
- 
+    if (handle == NULL) {
+        fprintf(stderr, "couldn't open device %s: %s\n", dev, errbuf);
+        return -1;
     }
 
-    printf("\n\n");
+    while (true) {
+        struct pcap_pkthdr* header;
+        const u_char* packet;
+        int res = pcap_next_ex(handle, &header, &packet);
 
-    printf("SOURCE MAC ADDRESS : ");
+        if (res == 0) continue;
 
-    for(int i=6; i < 12; i++) {
-        printf("%02x", packet[i]);
+        if (res == -1 || res == -2) break;
 
-        if(i != 11) {
-            printf(":");
-        }
+        printf("\n=================================================\n");
+        printf("%u bytes captured\n", header->caplen);
 
-        if(i!=0 && (i+1) %16 == 0){
-
-             printf("\n");
-        }
- 
-    }
-   
-    printf("\n\n");
-
-    printf("ETHER TYPE : 0x");
-
-    for(int i=12; i < 14; i++) {
-        printf("%02x", packet[i]);
-
-        if(i!=0 && (i+1) %16 == 0){
-
-             printf("\n");
-        }
- 
-    }
-
-
-    printf("\n\n");
-
-    printf("SOURCE IP ADDRESS : ");
-
-    for(int i=26; i < 30; i++) {
-        printf("%d", packet[i]);
+        struct ether_header* eth_header = (ether_header *)packet;
+        printMAC("Ethernet SMAC", eth_header->ether_shost);
+        printMAC("Ethernet DMAC", eth_header->ether_dhost);
+        printf("Ethernet TYPE: %04x\n", ntohs(eth_header->ether_type));
         
-        if(i != 29) {
-            printf(".");
-        }
- 
-        if(i!=0 && (i+1) %16 == 0){
+        packet += sizeof(struct ether_header);
+	     index += sizeof(struct ether_header);
 
-             printf("\n");
+        if(ntohs(eth_header->ether_type) == ETHERTYPE_IP) { // 이더타입이 아이피 타입일때만
+            struct ip* ip_header = (ip *)packet;
+            printf("IP SIP: %s\n", inet_ntoa(ip_header->ip_src));
+            printf("IP DIP: %s\n", inet_ntoa(ip_header->ip_dst));
+
+            packet += ip_header->ip_hl * 4;
+            index += ip_header->ip_hl * 4;
+
+            if(ip_header->ip_p == IPPROTO_TCP) {    // 아이피 헤더의 타입이 티씨피 일때만
+                struct tcphdr* tcp_header = (tcphdr *)packet;
+                printf("TCP SPORT: %d\n", ntohs(tcp_header->th_sport));
+                printf("TCP DPORT: %d\n", ntohs(tcp_header->th_dport));
+
+                packet += tcp_header->th_off * 4;
+		          index += tcp_header->th_off * 4;
+
+		
+               
+		          int count = 0;
+		          for(int i=index; i < header->caplen; i++) {
+        		      printf("%02x|", (int)packet[i]);
+
+        		      if(count!=0 && (count+1) %16 == 0){
+
+             		   printf("\n");
+        		      }
+
+			         count++;
+                }
+            }
         }
- 
     }
 
-    printf("\n\n");
-    printf("DESTINATION IP ADDRESS : ");
-   
-
-    for(int i=30; i < 34; i++) {
-        printf("%d", packet[i]); 
-
-        if(i != 33) {
-            printf(".");
-        }
-    }
-
-    printf("\n\n");
-
-    //해결해야할 문제 _ 각 바이트를 문자열을 합쳐서 해당 문자열을 정수형으로 표현한뒤 10진수로 표현해줘야함 
-    printf("SOURCE PORT : ");
-
-    int src_port_num;
-    src_port_num = MAKEWORD(packet[35], packet[34]);
-
-    printf("%d", src_port_num);
-
-    /*
-    for(int i=34; i < 36; i++) {
-        
-        printf("%02x", packet[i]); 
-    }
-    */
-
-    printf("\n\n");
-
-    printf("DESTINATION PORT : ");
-    
-    int dest_port_num;
-    dest_port_num = MAKEWORD(packet[37], packet[36]);
-
-    printf("%d", dest_port_num);
-  
-
-    printf("\n\n");
-
-    printf("==============================================================================");
-    printf("\n\n");
-    
-  }
-
-  pcap_close(handle);
-  return 0;
+    pcap_close(handle);
+    return 0;
 }
